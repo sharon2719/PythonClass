b23e7f0790756c140fca7c0bb0e6fbe743b01c1d
sharon2719 <otiennosharon@gmail.com>
2021-05-31 19:18:00 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 class Account:
 
     def __init__(self,name,phoneNumber,loan):
@@ -5,23 +6,42 @@ class Account:
         self.phoneNumber=phoneNumber
         self.balance=0
         self.loan=loan
+        self.statement=[]
 
     def showBalance(self):
         return f"Hello {self.name},your balance is kshs.{self.balance}."
 
     def deposit(self,amount):
-        self.balance+=amount
         if(amount<0):
             return f"You cannot deposit less than 0"
         else:
+            self.balance+=amount
+            now=datetime.now()
+            transaction={"amount":amount,"time":now,"narration":"You deposited"}
+            self.statement.append(transaction)
+           
             
             return self.showBalance()
+           
+    
+    def show_statement(self):
+        for transaction in self.statement:
+            amount=transaction["amount"]
+            narration=transaction["narration"]
+            time=transaction["time"]
+            date=time.strftime("%d/ %m / %y")
+            print(f"{date}:{narration}:{amount}")
+       
 
     def withdraw(self,amount):
         if(amount>self.balance):
             return f"Your balance is {self.balance}.You cannot withdraw {amount}"
         else:
             self.balance-=amount
+            now=datetime.now()
+            transaction={"amount":amount,"time":now,"narration":"You have withdrawn"}
+            self.statement.append(transaction)
+           
             return self.showBalance()
     
     def borrow(self,amount):
